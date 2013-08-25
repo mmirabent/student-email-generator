ea56709030a073e96d84d9104dd0539cffe8f9a5
Marcos Mirabent <mmirabent@gmail.com>
2013-08-25 19:07:11 -0400

    Refactored to eliminate depepdancy on roo. now using spreadsheet directly. Also added dynamic output, the output file will be placed next to the input file specified at the command line

--- student_email_generator.rb
+++ student_email_generator.rb
@@ -1,21 +1,12 @@
-require 'CSV'
-require 'tempfile'
-require 'roo'
+require "rubygems"
+require "spreadsheet"
+require "CSV"
 
-# If the current school year is 2013-2014, then the seniors graduate in 2014
+# get the absolute path for the first argument which should be a file name
+dataFile = File.expand_path(ARGV.first)
+outFile   = File.dirname(dataFile) + "/" + "Bulk Upload.csv"
 seniorGradYear = 2014
 emailDomain = "@pacespartans.com"
-
-# People like to have funny stuff like spaces, hyphens, apostrophes, etc
-# In their names. This fixes that
-def sanitize(name)
-  name.gsub /[\-\'\ \,\.]/, ""
-end
-
-# Temporary file for holding intermediate data. roo sucks, but I'm not about
-# to re-write it for this.
-tempFile  = Tempfile.new('export')
-outFile   = "out.csv"
 
 # Options for CSV::new
 options = {
@@ -25,37 +16,53 @@ options = {
   :force_quotes       => true
 }
 
-# Make the path string for the file
-dataFile = File.expand_path(ARGV.first);
+# turn arbitrary strings into symbols. Downcase, replace spaces with underscores
+# and finally, drop any characters that arent letters of underscores
+def symbolize(str)
+  str.downcase.gsub(/\ /, "_").gsub(/[^a-z_]/, "").to_sym
+end
 
-# Load and convert the .xls file to CSV
-dataExcel = Roo::Excel.new(dataFile)
-dataExcel.to_csv(tempFile)
+def sanitize(name)
+  name.gsub /[^a-zA-Z]/, ""
+end
 
-# Instantiate the csv object for dealing with the csv file and add a header
+workbook = Spreadsheet.open(dataFile)
+worksheet = workbook.worksheet 0
+
+# Find header row and prepare header hash
+headerRow = worksheet.row worksheet.dimensions[0]
+headerHash = Hash.new
+
+# Process header row
+headerRow.each_index do |cell_index|
+  headerHash[symbolize headerRow[cell_index]] = cell_index
+end
+
+# Open the CSV file for writing
 CSV.open(outFile, "wb",options) do |outCSV|
+  # write out the header file
   outCSV << ["email address", "first name", "last name", "password"]
-  
-  # This writes out each of the rows on our output.csv
-  # Google Apps is expecting Email, First Name, Last Name, Password
-  # as headers in a .csv
-  # APID is defined as GGNNN where GG is the students current grade. GG is 
-  # extracted by integer division. GGNNN / 1000 = GG
-  # the student number is extracted by modulus GGNNN % 1000 = NNN
-  # To get graduation year from grade level, take the year the current seniors
-  # will graduate at, add 12 and subtract the current grade level
-  CSV.foreach(tempFile,options) do |row|
-    firstName   = row.field(:first_name)
-    lastName    = row.field(:last_name)
-    password    = row.field(:birth_date)
-    apid        = row.field(:apid).to_i
+
+  # Begin iterating after the header row
+  worksheet.each(worksheet.dimensions[0]+1) do |row|
+    # This writes out each of the rows on our output.csv
+    # Google Apps is expecting Email, First Name, Last Name, Password
+    # as headers in a .csv
+    # APID is defined as GGNNN where GG is the students current grade. GG is 
+    # extracted by integer division. GGNNN / 1000 = GG
+    # the student number is extracted by modulus GGNNN % 1000 = NNN
+    # To get graduation year from grade level, take the year the current seniors
+    # will graduate at, add 12 and subtract the current grade level
+    firstName   = row[headerHash[:first_name]]
+    lastName    = row[headerHash[:last_name]]
+    birth_date  = row[headerHash[:birth_date]]
+    apid        = row[headerHash[:apid]].to_i
+    password    = birth_date.strftime("%m-%d-%Y")
     grad_year   = (seniorGradYear % 1000 + 12 - apid / 1000).to_s
     student_no  = "%03d" % (apid % 1000)
-    
+
     email = firstName[0] + sanitize(lastName) + grad_year + student_no + emailDomain
-    
+
     outCSV << [email, firstName, lastName, password]
   end
 end
-
-tempFile.close!
